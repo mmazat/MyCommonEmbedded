8f85fa0776bc79f3c630378260e660c5db91cb65
mehdi <m.mazaheri.t@gmail.com>
2020-01-11 18:03:43 -0500

    add time

--- MyCommonBlynk.h
+++ MyCommonBlynk.h
@@ -1,15 +1,22 @@
+#include <Time.h>
 #include <TimeLib.h>
 #include <WidgetRTC.h>
 
 
 WidgetRTC widget_rtc;
+
+BLYNK_CONNECTED() {
+  // Synchronize time on connection
+  widget_rtc.begin();
+}
+
 // Digital clock display of the time
 String getDateAndTime()
 {
   // You can call hour(), minute(), ... at any time
   // Please see Time library examples for details
   char str[35];
-  sprintf(str,"%02d/%02d/%02d %02d:%02d:%02d\0",year(),month(),day(), hour(),minute(),seconds());
+  sprintf(str,"%02d/%02d/%02d %02d:%02d:%02d\0",day(),month(),year(),hour(),minute(),second());
   
   return String(str);
 
